9572e333ad1458a3f927d1e4cbf79dfc8f0764b3
BaoquocF1804 <s2chungbeov2@gmail.com>
2023-06-29 10:25:20 +0700

    change_3.0

--- service_account/server/rpc_create_account.go
+++ service_account/server/rpc_create_account.go
@@ -20,6 +20,13 @@ func (server *Server) CreateAccount(ctx context.Context, req *pb_account.CreateA
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create account: %s", err)
 	}
-
-	return nil, nil
+	account := pb_account.Account{
+		Owner:    arg.Owner,
+		Balance:  arg.Balance,
+		Currency: arg.Currency,
+	}
+	rps := &pb_account.CreateAccountResponse{
+		Account: &account,
+	}
+	return rps, nil
 }
